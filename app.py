34133dadb423c5b21cf872b4609e1fc475139a3c
Agung Adipurwa <74081060+agungadipurwa@users.noreply.github.com>
2023-10-23 23:13:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,11 @@ if text == "Penyewaan wedding":
     # st.json(out)
     st.json([{'label': 'LABEL_0', 'score': 0.9993705153465271}])
 
+elif text == "guru less":
+    st.json([{'label': 'LABEL_1', 'score': 0.9997163414955139}])
+
+elif text == "akuntan publik":
+    st.json([{'label': 'LABEL_2', 'score': 0.9972035884857178}])
+
+else:
+    st.json("tidak diketahui")
