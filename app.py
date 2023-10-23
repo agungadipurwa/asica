d9224e77e92347ecdad14b890b7bc6efe86d6488
Agung Adipurwa <74081060+agungadipurwa@users.noreply.github.com>
2023-10-23 22:51:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from transformers import pipeline
 
-pipe = pipeline("sentiment-analysis", model="./model/")
+#pipe = pipeline("sentiment-analysis", model="./model/")
 
 text = st.text_area('Masukan teks (contoh: Penyewaan wedding)')
 
